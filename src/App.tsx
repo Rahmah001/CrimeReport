63215706eac3d6345a908b0d1d7e2cae22a1f4d9
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-15 22:23:10 +0100

    chore(ui): App UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home/Home'
+import Home from './pages/Home/Home';
 import About from './pages/About/About';
-
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;

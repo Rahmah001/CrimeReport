ba5235c21a1ea499bdd161cde7c3289b611fb02e
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-15 23:51:19 +0100

    chore(ui): Home Page Structure

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,62 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Button, Heading, HStack, Text } from '@chakra-ui/react';
 
-import { overlayBg } from '@constants/index';
+import {
+  bgGradient,
+  buttonGradient,
+  headingResSize,
+  overlayBg,
+} from '@constants/index';
 
 import Navbar from '@components/Navbar/Navbar';
+import MiddleSection from '@components/MiddleSection/MiddleSection';
 
 const Home = () => {
   return (
-    <Box h={'500px'} bg={overlayBg} bgSize={'cover'}>
-      <Navbar />
-      <h2>Home</h2>
+    <Box>
+      <Box h={{ base: '500px', sm: '600px' }} bg={overlayBg} bgSize={'cover'}>
+        <Navbar />
+        <Box
+          textAlign={'center'}
+          mt={{ base: '8rem', sm: '12rem' }}
+          color={'white'}
+        >
+          <Heading fontWeight={'extrabold'} size={headingResSize}>
+            Dont Feel Helpless,
+          </Heading>
+          <Heading
+            bgGradient={bgGradient}
+            bgClip={'text'}
+            fontWeight={'extrabold'}
+            size={headingResSize}
+            mt={{ base: 0, sm: 2 }}
+          >
+            we are here for you.
+          </Heading>
+
+          <Text mt={{ base: 0, sm: 4 }}>
+            Is there any crime happening around you
+          </Text>
+          <Text>or your loved ones we're one call away.</Text>
+        </Box>
+
+        <HStack spacing={3} justify={'center'} mt={7}>
+          <Button
+            bgGradient={bgGradient}
+            _hover={buttonGradient}
+            _focus={buttonGradient}
+            size={'sm'}
+            color={'#000'}
+            colorScheme="blue"
+          >
+            Report crime
+          </Button>
+          <Button variant={'outline'} size={'sm'} colorScheme="whiteAlpha">
+            Committed to safety
+          </Button>
+        </HStack>
+      </Box>
+
+      <MiddleSection />
     </Box>
   );
 };

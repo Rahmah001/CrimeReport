79fd57a7fa0a5a7a2ccbe8501ab9c43d45eec7f2
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-15 21:28:29 +0100

    chore(ui): register font in chakra-ui provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 import App from './App'
 
 import './index.css'
 
+const theme = extendTheme({
+  fonts: {
+    body: 'Albert Sans',
+    heading: 'Albert Sans',
+    mono: 'Albert Sans',
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Router>
         <App />
       </Router>

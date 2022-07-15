2d499a05d5f04b389903e5cac227af4af653b480
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-15 20:24:57 +0100

    refactor: file imports in order

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { ChakraProvider } from '@chakra-ui/react';
+
 import App from './App'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(

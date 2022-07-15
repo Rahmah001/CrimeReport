5de2d35b43be9b5a6c9c0991f9d4937f982f496c
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-16 00:19:32 +0100

    chore(ui): crimeSection UI

---
+++ src/components/CrimeSection/CrimeSection.tsx
@@ -0,0 +1,16 @@
+import { Box, Heading } from '@chakra-ui/react';
+
+const CrimeSection = () => {
+  return (
+    <Box textAlign={'center'}>
+      <Heading fontWeight={'extrabold'} fontSize={'2xl'}>
+        We are here for any form of
+      </Heading>
+      <Heading color={'#385898'} fontWeight={'extrabold'} size={'2xl'}>
+        crime incidence.
+      </Heading>
+    </Box>
+  );
+};
+
+export default CrimeSection;

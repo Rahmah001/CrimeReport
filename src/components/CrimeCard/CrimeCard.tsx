c9fdd24ee86a20b1aef5ea04e80496e2b33b8594
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-16 00:42:05 +0100

    chore(ui):  CrimeCard UI

---
+++ src/components/CrimeCard/CrimeCard.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react';
+import { CrimeCardProps } from 'src/interfaces';
+
+const CrimeCard: React.FC<CrimeCardProps> = ({ crimeData }) => {
+  return (
+    <Box>
+      <Text>{crimeData?.crimeTitle}</Text>
+    </Box>
+  );
+};
+
+export default CrimeCard;

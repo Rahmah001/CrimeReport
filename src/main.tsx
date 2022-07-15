5374d101752c1723ac29b85807eedd9f0a72da77
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-15 21:19:22 +0100

    chore: added react-router, to enable routing across app

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import { ChakraProvider } from '@chakra-ui/react';
 
@@ -10,7 +11,9 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ChakraProvider>
   </React.StrictMode>
 )

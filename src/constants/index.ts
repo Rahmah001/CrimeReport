c64eb4147044277b81c2ac178a5f8f397cb8c6b8
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-15 23:27:25 +0100

    chore(style): Added buttonGradientColor in constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,10 @@
 import CrimeBg from '@assets/images/crime-bg.jpg';
 
 export const overlayBg = `linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),url('${CrimeBg}') no-repeat center`;
+
+export const buttonGradient = {
+  bgGradient: 'linear(to-b, orange.100, purple.300)',
+  color: '#000',
+};
+
+export const bgGradient = 'linear(to-b, orange.100, purple.300)';

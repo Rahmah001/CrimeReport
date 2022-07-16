cd26c666212f9afc13ca942be14903451021f9fa
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-16 01:32:23 +0100

    chore: prettier formats

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,26 +1,26 @@
-import { Container, FormControl, FormLabel, Input } from "@chakra-ui/react"
+import { Container, FormControl, FormLabel, Input } from '@chakra-ui/react';
 
- const ContactSection = () => {
+const ContactSection = () => {
   return (
     <Container maxWidth={'md'} mt={'3rem'}>
-          <FormControl>
-            <FormLabel htmlFor="name">Name</FormLabel>
-            <Input id="name" type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="phoneNumber">Phone number</FormLabel>
-            <Input id="phoneNumber" type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="name">Name</FormLabel>
-            <Input id="name" type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="email">Email</FormLabel>
-            <Input id="email" type="email" />
-          </FormControl>
-        </Container>
-  )
-}
+      <FormControl>
+        <FormLabel htmlFor="name">Name</FormLabel>
+        <Input id="name" type="text" />
+      </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="phoneNumber">Phone number</FormLabel>
+        <Input id="phoneNumber" type="text" />
+      </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="name">Name</FormLabel>
+        <Input id="name" type="text" />
+      </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="email">Email</FormLabel>
+        <Input id="email" type="email" />
+      </FormControl>
+    </Container>
+  );
+};
 
-export default ContactSection
+export default ContactSection;

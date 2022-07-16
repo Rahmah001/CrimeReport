019b57547eac631ca8acddbc4530dbe00847c51d
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-16 11:42:26 +0100

    chore(ui): Added ScrollTop to root app

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import { ChakraProvider } from '@chakra-ui/react';
 
-import App from './App'
+import App from './App';
+import ScrollTop from '@components/ScrollTop/ScrollTop';
+
 import { theme } from './assets/theme';
 
-import './index.css'
-
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <Router>
         <App />
+        <ScrollTop />
       </Router>
     </ChakraProvider>
   </React.StrictMode>
-)
+);

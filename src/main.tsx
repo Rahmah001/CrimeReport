27d1f6593de4f11d5a5c0cbff5eaf6dc2d53165d
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-16 14:45:44 +0100

    chore: removed route provider from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import { ChakraProvider } from '@chakra-ui/react';
 
 import ScrollTop from '@components/ScrollTop/ScrollTop';
+
 import { theme } from '@assets/theme';
 
 import App from './App';
@@ -14,10 +14,8 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <Router>
-        <App />
-        <ScrollTop />
-      </Router>
+      <App />
+      <ScrollTop />
     </ChakraProvider>
   </React.StrictMode>
 );

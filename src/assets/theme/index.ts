bdac94ff31bfc14bf801e282e4c1146f3b7fd4a6
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-16 06:43:13 +0100

    chore(theme): override default focusBorderColor for Input and Select component respectively

--- src/assets/theme/index.ts
+++ src/assets/theme/index.ts
@@ -1,4 +1,4 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
   fonts: {
@@ -6,4 +6,16 @@ export const theme = extendTheme({
     heading: 'Albert Sans',
     mono: 'Albert Sans',
   },
-})
+  components: {
+    Input: {
+      defaultProps: {
+        focusBorderColor: 'facebook.500',
+      },
+    },
+    Select: {
+      defaultProps: {
+        focusBorderColor: 'facebook.500',
+      },
+    },
+  },
+});

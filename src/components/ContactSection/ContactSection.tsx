6855deb890a145f4bfeba37b502240ac9824e0ea
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-16 01:59:15 +0100

    chore(ui): replaced HStack with Stack for responsiveness

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,24 +1,56 @@
-import { Container, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Stack,
+} from '@chakra-ui/react';
+import { bgGradient, buttonGradient } from 'src/constants';
 
 const ContactSection = () => {
   return (
-    <Container maxWidth={'md'} mt={'3rem'}>
-      <FormControl>
-        <FormLabel htmlFor="name">Name</FormLabel>
-        <Input id="name" type="text" />
-      </FormControl>
-      <FormControl>
-        <FormLabel htmlFor="phoneNumber">Phone number</FormLabel>
-        <Input id="phoneNumber" type="text" />
-      </FormControl>
-      <FormControl>
-        <FormLabel htmlFor="name">Name</FormLabel>
-        <Input id="name" type="text" />
-      </FormControl>
-      <FormControl>
-        <FormLabel htmlFor="email">Email</FormLabel>
-        <Input id="email" type="email" />
-      </FormControl>
+    <Container maxWidth={'xl'} mt={'3rem'}>
+      <Stack direction={{ base: 'column', sm: 'row' }} spacing={5}>
+        <FormControl>
+          <FormLabel htmlFor="name">Name</FormLabel>
+          <Input id="name" type="text" />
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="phoneNumber">Phone number</FormLabel>
+          <Input id="phoneNumber" type="text" />
+        </FormControl>
+      </Stack>
+      <Stack direction={{ base: 'column', sm: 'row' }} spacing={5} my={4}>
+        <FormControl>
+          <FormLabel htmlFor="crime">Select crime</FormLabel>
+          <Select id="crime" placeholder="Select crime">
+            <option>Armed Robbery</option>
+            <option>Murder</option>
+            <option>Kidnapping</option>
+            <option>Bugling</option>
+            <option>Hijacking</option>
+            <option>Accident</option>
+          </Select>
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <Input id="email" type="email" />
+        </FormControl>
+      </Stack>
+      <Button
+        mt={3}
+        type={'submit'}
+        bgGradient={bgGradient}
+        _hover={buttonGradient}
+        _focus={buttonGradient}
+        size={'sm'}
+        color={'#000'}
+        colorScheme="blue"
+      >
+        Submit now!
+      </Button>
     </Container>
   );
 };

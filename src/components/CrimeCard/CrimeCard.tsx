f98e1fa2c1c17e6bb512ef4d0908b5d9e69db802
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-16 06:31:25 +0100

    chore(ui): destructure stuff from props

--- src/components/CrimeCard/CrimeCard.tsx
+++ src/components/CrimeCard/CrimeCard.tsx
@@ -3,18 +3,19 @@ import { Box, Text } from '@chakra-ui/react';
 import { CrimeCardProps } from 'src/interfaces';
 
 const CrimeCard: React.FC<CrimeCardProps> = ({ crimeData }) => {
+  const { crimeImage, crimeText, crimeTitle } = crimeData;
   return (
     <Box
       p={8}
       borderRadius={'10px'}
-      bg={`linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5)),url('${crimeData?.crimeImage}') no-repeat center`}
+      bg={`linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5)),url('${crimeImage}') no-repeat center`}
       bgSize={'cover'}
       color={'white'}
     >
       <Text fontSize={'xl'} fontWeight={'semibold'}>
-        {crimeData?.crimeTitle}
+        {crimeTitle}
       </Text>
-      <Text mt={3}>{crimeData?.crimeText}</Text>
+      <Text mt={3}>{crimeText}</Text>
     </Box>
   );
 };

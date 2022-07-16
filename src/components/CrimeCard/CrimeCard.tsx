f57550f3364d4494bdaa378647bc28682a4415ae
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-16 07:01:58 +0100

    styles: added styles for crimeCard

--- src/components/CrimeCard/CrimeCard.tsx
+++ src/components/CrimeCard/CrimeCard.tsx
@@ -1,4 +1,5 @@
 import { Box, Text } from '@chakra-ui/react';
+import { buttonGradient } from 'src/constants';
 
 import { CrimeCardProps } from 'src/interfaces';
 
@@ -8,11 +9,16 @@ const CrimeCard: React.FC<CrimeCardProps> = ({ crimeData }) => {
     <Box
       p={8}
       borderRadius={'10px'}
-      bg={`linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5)),url('${crimeImage}') no-repeat center`}
+      bg={`linear-gradient(0deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.5)),url('${crimeImage}') no-repeat center`}
       bgSize={'cover'}
       color={'white'}
     >
-      <Text fontSize={'xl'} fontWeight={'semibold'}>
+      <Text
+        fontSize={'2xl'}
+        bgGradient={buttonGradient.bgGradient}
+        bgClip={'text'}
+        fontWeight={'bold'}
+      >
         {crimeTitle}
       </Text>
       <Text mt={3}>{crimeText}</Text>

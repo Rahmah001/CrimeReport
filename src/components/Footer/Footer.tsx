bc1fcb73fa76ef4fba054c350cd4612eb43448e5
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-16 13:43:07 +0100

    chore(ui); moved newsletter section to it's own component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,20 +1,8 @@
-import {
-  Box,
-  Button,
-  Container,
-  FormControl,
-  Heading,
-  Input,
-  ListItem,
-  SimpleGrid,
-  Text,
-  UnorderedList,
-} from '@chakra-ui/react';
+import { Box, Container, Heading, Text } from '@chakra-ui/react';
 
 import FooterBg from '@assets/images/footer-bg.jpg';
-import { Link } from 'react-router-dom';
 
-import { bgGradient, buttonGradient } from 'src/constants';
+import NewsLetter from '@components/NewsLetter/NewsLetter';
 
 const Footer = () => {
   return (
@@ -36,80 +24,7 @@ const Footer = () => {
       </Container>
 
       <Container maxWidth={'container.md'}>
-        <SimpleGrid
-          px={{ base: 6, sm: 0 }}
-          spacing={5}
-          mt={'4rem'}
-          columns={{ base: 2, sm: 3 }}
-        >
-          <Box>
-            <Text fontWeight={'medium'} fontSize={'xl'} color={'#C6F6D5'}>
-              Our Support
-            </Text>
-            <UnorderedList mt={2} color={'white'}>
-              <ListItem>
-                <Link to={'#'}>Advanced</Link>
-              </ListItem>
-              <ListItem>
-                <Link to={'#'}>Management</Link>
-              </ListItem>
-              <ListItem>
-                <Link to={'#'}>Corporate</Link>
-              </ListItem>
-              <ListItem>
-                <Link to={'#'}>Customer</Link>
-              </ListItem>
-              <ListItem>
-                <Link to={'#'}>Information</Link>
-              </ListItem>
-            </UnorderedList>
-          </Box>
-          <Box>
-            <Text fontWeight={'medium'} fontSize={'xl'} color={'#C6F6D5'}>
-              Quick Link
-            </Text>
-            <UnorderedList mt={2} color={'white'}>
-              <ListItem>
-                <Link to={'#'}>New Law</Link>
-              </ListItem>
-              <ListItem>
-                <Link to={'#'}>About</Link>
-              </ListItem>
-              <ListItem>
-                <Link to={'#'}>Privacy Policy</Link>
-              </ListItem>
-              <ListItem>
-                <Link to={'#'}>Licenses</Link>
-              </ListItem>
-            </UnorderedList>
-          </Box>
-          <Box color={'white'} width={{ base: '280px', sm: 'inherit' }}>
-            <Heading fontSize={'lg'}>Newsletter</Heading>
-            <Text fontSize={'14px'} mt={3}>
-              Subscribe now to get daily updates
-            </Text>
-            <FormControl my={3}>
-              <Input
-                size={'sm'}
-                bgColor={'white'}
-                borderRadius={'md'}
-                id="email"
-                type="email"
-                placeholder={'Email Address'}
-              />
-            </FormControl>
-            <Button
-              type={'submit'}
-              bgGradient={bgGradient}
-              _hover={buttonGradient}
-              _focus={buttonGradient}
-              color={'#000'}
-              size={'sm'}
-            >
-              Subscribe
-            </Button>
-          </Box>
-        </SimpleGrid>
+        <NewsLetter />
       </Container>
     </Box>
   );

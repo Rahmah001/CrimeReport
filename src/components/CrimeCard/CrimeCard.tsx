7bfbb2c744743e57bc03bd76d1722e5bfa25d67f
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-16 01:05:06 +0100

    chore(ui): crimeCard composition

--- src/components/CrimeCard/CrimeCard.tsx
+++ src/components/CrimeCard/CrimeCard.tsx
@@ -4,8 +4,16 @@ import { CrimeCardProps } from 'src/interfaces';
 
 const CrimeCard: React.FC<CrimeCardProps> = ({ crimeData }) => {
   return (
-    <Box>
-      <Text>{crimeData?.crimeTitle}</Text>
+    <Box
+      p={6}
+      borderRadius={'10px'}
+      bg={`linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5)),url('${crimeData?.crimeImage}') no-repeat center`}
+      bgSize={'cover'}
+    >
+      <Text fontSize={'xl'} fontWeight={'semibold'}>
+        {crimeData?.crimeTitle}
+      </Text>
+      <Text>{crimeData?.crimeText}</Text>
     </Box>
   );
 };

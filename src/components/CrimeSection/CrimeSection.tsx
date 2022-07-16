b10c043f558f560fc166b1ba7390ddfcafbb4fe3
AbdulAzeez Olamide <abdulazeezolamide50@gmail.com>
2022-07-16 01:15:35 +0100

    style: crime-section component styling

--- src/components/CrimeSection/CrimeSection.tsx
+++ src/components/CrimeSection/CrimeSection.tsx
@@ -6,18 +6,23 @@ import { crimeDataArray } from 'src/constants';
 
 const CrimeSection = () => {
   return (
-    <Box textAlign={'center'}>
-      <Heading fontWeight={'extrabold'} fontSize={'2xl'}>
+    <Box bgGradient={'linear(to-b, orange.100, purple.300)'} py={14}>
+      <Heading textAlign={'center'} fontWeight={'extrabold'} fontSize={'2xl'}>
         We are here for any form of
       </Heading>
-      <Heading color={'#385898'} fontWeight={'extrabold'} size={'2xl'}>
+      <Heading
+        textAlign={'center'}
+        color={'#385898'}
+        fontWeight={'extrabold'}
+        size={'2xl'}
+      >
         crime incidence.
       </Heading>
 
-      <Container maxW={'container.lg'} mt={'4rem'}>
-        <SimpleGrid spacing={4} columns={{ base: 1, sm: 2, md: 3 }}>
+      <Container maxW={'container.lg'} my={'4rem'}>
+        <SimpleGrid spacing={5} columns={{ base: 1, sm: 2, md: 3 }}>
           {crimeDataArray.map((crimeData) => (
-            <CrimeCard crimeData={crimeData} />
+            <CrimeCard key={crimeData.id} crimeData={crimeData} />
           ))}
         </SimpleGrid>
       </Container>
